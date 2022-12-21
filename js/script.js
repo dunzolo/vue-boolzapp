f05e22400d926d0d794630bc3d00130091fea0b1
dunzolo <rossidavide108@gmail.com>
2022-12-21 12:49:37 +0100

    added chat sidebar

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: '_1',
+                    avatar: 'avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -30,7 +30,7 @@ createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: '_2',
+                    avatar: 'avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -52,7 +52,7 @@ createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: '_3',
+                    avatar: 'avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -74,7 +74,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: '_4',
+                    avatar: 'avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -91,7 +91,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: '_5',
+                    avatar: 'avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -108,7 +108,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: '_6',
+                    avatar: 'avatar_6.jpg',
                     visible: true,
                     messages: [
                     {
